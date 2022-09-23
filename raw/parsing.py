dd3d07053188593f3709ab04a25dcaee1b2b2ae0
MuffinLinwist <carlosbarrientosugarte97@gmail.com>
2022-09-23 15:23:23 +0200

    Update parsing.py

--- raw/parsing.py
+++ raw/parsing.py
@@ -6,7 +6,6 @@ def parse_line(number, text, ddct):
         if char not in "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789":
             ddct[char] += [(number, text[i - 5:i + 20])]
 
-
 ddct = defaultdict(list)
 text = open('raw_oliveira.txt', 'r', encoding="utf8").read()
 text = text.split("\n")
@@ -14,4 +13,4 @@ for l in text:
     start = l.split(".")[0]
     parse_line(start, l, ddct)
 for k, v in sorted(ddct.items(), key=lambda x: len(x[1]), reverse=True):
-    print(k, "\t", hex(chr(k)), "\t", v[0][0], "\t", v[0][1])  # first context observed!)
+    print(k, "\t", hex(ord(k)), "\t", v[0][0], "\t", v[0][1])
