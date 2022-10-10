f65ef8abea866f4248c6d3c2a97f3797d6096ceb
MuffinLinwist <carlosbarrientosugarte97@gmail.com>
2022-10-10 18:16:46 +0200

    Update formating.py

--- raw/formating.py
+++ raw/formating.py
@@ -1,19 +1,43 @@
 from collections import defaultdict
-
+import re
 
 def block(number, text, ddct):
     #print(text)
     concept_entries = []
     line_list = text.split(":")
-    #print(line_list)
-    for entry in line_list:
-        #print(entry)
-        concept_entries.append(entry)
-        header = concept_entries[0]
-        for i in concept_entries:
-            blocks = zip(header, concept_entries[i])
-            print(blocks)
+    # print(line_list)
+    # print(number)
+    identifier = line_list[0]
+    #print(identifier)
+    entry_list = []
+    for line in line_list:
+        #print(line)
+        if line == identifier:
+            protoform = line.split("‘")[0]
+            protoform = protoform.split("(")[0]
+            #print(protoform)
+            pass
+        else:
+            lang = line.split(" ")
+            #print(lang)
+            while ("" in lang):
+                lang.remove("")
+                #print(lang)
 
+
+            #if "‘" in line:
+                #print(line)
+        #source = line[line.find("(") + 1:line.find(")")]
+        # print(source)
+        # if not source:
+           # source = ''
+
+
+        # print(f"{protoform} || {concept} || {source} ")
+    #    for entry in line_list:
+    #        print(entry)
+    #        concept_entries.append(entry)
+    #        break
 
 
 ddct = defaultdict(list)
@@ -21,11 +45,12 @@ text = open('raw_oliveira.txt', 'r', encoding="utf8").read()
 text = text.split("\n")
 #print(text)
 for l in text:
-    start = l.split(".")[0]
-    block(start, l, ddct)
+    l_splitted = l.split(".")
+    start = l_splitted[0]
+    final_entry = l_splitted[1:]
+    block(start, "".join(final_entry), ddct)
 
-
-
+    # break
 
 
 # parse(text)
