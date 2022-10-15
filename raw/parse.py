fae0315b71fa8eaa0becdb319fe437517d7a9cde
lingulist <mattis.list@lingulist.de>
2022-10-15 20:24:00 +0200

    update

--- raw/parse.py
+++ raw/parse.py
@@ -9,26 +9,35 @@ with open("replacements.txt") as f:
         if a != "CHAR":
             rep[eval('"'+a+'"')] = b.strip()
 
+with open("../etc/languages.tsv") as f:
+    langs = {}
+    for row in f:
+        ents = row.split("\t")
+        langs[ents[1]] = ents[0]
+
 # load the data
 bad_lines = []
 bad_entries = []
+oid = 1
 with open("raw_oliveira-mod.txt") as f:
     data = OrderedDict()
     for line in f:
         line = "".join([rep.get(c, c) for c in line])
         # get first parts with indices
-        idx, rest = line[:line.index(".")], line[line.index("."):].strip()
+        idx, rest = line[:line.index(".")], line[line.index(".")+1:].strip()
         
         # we ignore lines that are difficult (two so far)
         if idx.startswith("!"):
             pass
         else:
             # start by separating proto-forms
-            proto, rest = rest[:line.index(" : ")].strip(), rest[rest.index(" : ")+3:]
+            proto, rest = rest[:line.index(" : ")-3].strip(), rest[rest.index(" : ")+3:]
 
             # proto-form and cocept
             if "‘" in proto:
                 pform, pconcept = proto.strip()[:-1].split("‘")
+                pconcept = pconcept.strip().strip("’")
+                pform = pform.strip()
                 if ":" in pconcept:
                     bad_lines += [idx]
             else:
@@ -37,30 +46,47 @@ with open("raw_oliveira-mod.txt") as f:
             print(idx, pform, pconcept)
             # split entries now to get individual entries here with regexes
             for entry in rest.split(" : "):
-                if ":" in entry or not " " in entry:
+                if (":" in entry and not "::" in entry) or not " " in entry:
                     bad_entries += [(idx, entry)]
                 else:
+                    entry = entry.replace("::", ":")
                     bad_entry = False
                     language = entry[:entry.index(" ")]
-                    erest = entry[entry.index(" "):].strip()
-                    if "," in erest:
-                        erests = [x.strip() for x in erest.split(",")]
-                    else:
-                        erests = [erest]
-                    for erest in erests:
-                        if "‘" in erest:
-                            try:
-                                form, concept = erest.split("‘")
-                                form = form.strip()
-                                concept = concept.strip()
-                            except:
-                                bad_entries += [(idx, entry)]
-                                bad_entry = True
+                    if language in langs:
+                        erest = entry[entry.index(" "):].strip()
+                        if "," in erest:
+                            erests = [x.strip() for x in erest.split(",")]
                         else:
-                            form = erest
-                            concept = pconcept
-                        if not bad_entry:
-                            print(idx, language, form, concept)
+                            erests = [erest]
+                        for erest in erests:
+                            erest = erest.replace(";;", ",")
+                            if "‘" in erest:
+                                try:
+                                    form, concept = erest.split("‘")
+                                    form = form.strip()
+                                    if concept.endswith("’"):
+                                        concept = concept[:-1]
+                                        note = ""
+                                    else:
+                                        if "’" in concept:
+                                            note = concept.split("’")[1]
+                                        else:
+                                            note = ""
+                                except:
+                                    bad_entries += [(idx, entry)]
+                                    bad_entry = True
+                            else:
+                                form = erest
+                                concept = "**"+pconcept
+                                note = ""
+                            if not bad_entry:
+                                data[oid] = [idx, langs[language], form, concept,
+                                        note, pform,
+                                        pconcept, entry]
+                                print(idx, language, form, concept)
+                                oid += 1
+                    else:
+                        bad_entries += [(idx, entry)]
 
                         
 print("")
@@ -77,4 +103,10 @@ print("Number | Line ID | Entry\n--- | --- | ---")
 for i, (a, b) in enumerate(bad_entries):
     print("{0} | {1:20} | {2}".format(i+1, a, b))
 
-
+with open("parsed-entries.tsv", "w") as f:
+    for idx, vals in data.items():
+        if vals[2].strip() in ["--", "--"] or not vals[2].strip():
+            pass
+        else:
+            f.write(str(idx)+"\t"+"\t".join(vals)+"\n")
+        
