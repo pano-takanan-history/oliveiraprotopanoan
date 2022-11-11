1f08c8c2de42790ffceb9c27f60ae84e6d897cc7
Frederic Blum <frederic_blum@eva.mpg.de>
2022-11-11 12:52:58 +0100

    update notes and setup

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("metadata.json", encoding="utf-8") as fp:
     metadata = json.load(fp)
 
 setup(
-    name='lexibank_aaleykusunda',
-    py_modules=['lexibank_aaleykusunda'],
+    name='lexibank_oliveiraprotopanoan',
+    py_modules=['lexibank_oliveiraprotopanoan'],
     include_package_data=True,
     url=metadata.get("url",""),
     zip_safe=False,
     entry_points={
         'lexibank.dataset': [
-            'aaleykusunda=lexibank_aaleykusunda:Dataset',
+            'oliveiraprotopanoan=lexibank_oliveiraprotopanoan:Dataset',
         ]
     },
     install_requires=[
