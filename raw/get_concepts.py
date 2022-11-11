77ab6c7fc339b99bf8bf1da4ed89030067734c3e
Frederic Blum <frederic_blum@eva.mpg.de>
2022-11-11 13:02:22 +0100

    update concepts

--- raw/get_concepts.py
+++ raw/get_concepts.py
@@ -1,14 +1,26 @@
+from collections import defaultdict
 from lingpy import *
 from pysem.glosses import to_concepticon
 
 wl = Wordlist("parsed-entries.tsv")
 
 with open("../etc/concepts.tsv", "w") as f:
-    f.write("NUMER\tSPANISH\tCONCEPTICON_ID\tCONCEPTICON_GLOSS\n")
+    f.write("NUMER\tSPANISH\tCONCEPTICON_ID\tCONCEPTICON_GLOSS\tProto-Concept\n")
+
+    proto_concepts = []
+    for i in wl:
+        if wl[i, "protoconcept"] not in proto_concepts:
+            proto_concepts.append(wl[i, "protoconcept"])
+
     for i, concept in enumerate(wl.concepts):
-        mapped = to_concepticon([{"gloss": concept}], language="es")
-        if mapped[concept]:
-            cid, cgl = mapped[concept][0][:2]
+        if concept in proto_concepts:
+            is_proto = 1
+            mapped = to_concepticon([{"gloss": concept}], language="es")
+
+            if mapped[concept]:
+                cid, cgl = mapped[concept][0][:2]
         else:
-            cid, cgl = "", ""
-        f.write("{0}\t{1}\t{2}\t{3}\n".format(i+1, concept, cid, cgl))
+            cid, cgl, is_proto = "", "", ""
+
+        # print(wl.concepts[i])
+        f.write("{0}\t{1}\t{2}\t{3}\t{4}\n".format(i+1, concept, cid, cgl, is_proto))
