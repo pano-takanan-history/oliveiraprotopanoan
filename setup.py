35eae367cf86681d77496f07a1c10bd5babb4a04
Christoph Rzymski <christoph@functor-argument.net>
2023-06-15 11:36:38 +0200

    Release prep (#42)
    * Remove pinned dependencies
    
    * Require pyedictor
    
    * Update setup metadata
    
    * Add GH actions
    
    * Add tests
    
    * Re-generate & update contributors
    
    * remove files unrelated to dataset
    
    ---------
    
    Co-authored-by: Frederic Blum <frederic_blum@eva.mpg.de>

--- setup.py
+++ setup.py
@@ -6,6 +6,8 @@ with open("metadata.json", encoding="utf-8") as fp:
 
 setup(
     name='lexibank_oliveiraprotopanoan',
+    description=metadata["title"],
+    license=metadata.get("license", ""),
     py_modules=['lexibank_oliveiraprotopanoan'],
     include_package_data=True,
     url=metadata.get("url",""),
@@ -17,10 +19,7 @@ setup(
     },
     install_requires=[
         "pylexibank>=3.0.0",
-        "cldfbench==1.13.0",
-        "pycldf==1.29.0",
-        "csvw==3.1.3",
-        "pyconcepticon==3.0.0",
+        "pyedictor",
     ],
     extras_require={
         'test': [
